65ce9dff786d72d96b04700671b4df27cf606746
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-04-20 14:20:23 +0200

    Overhaul structure + add weight init

--- src/models.py
+++ src/models.py
@@ -1,7 +1,9 @@
 import pytorch_lightning as pl
 import torch
+from torch import nn
 import torchvision
 import torch.nn.functional as F
+
 from deep_convolutional_model import Generator, Discriminator
 from helpers import randomly_flip_labels
 from naive_model import NaiveGenerator, NaiveDiscriminator
@@ -20,6 +22,9 @@ class conditionalGAN(pl.LightningModule):
         num_features,
         lr,
         batch_size,
+        label_flipping_p,
+        b1=0.5,
+        b2=0.99,
         condition=True,
         ** kwargs
     ):
@@ -28,10 +33,10 @@ class conditionalGAN(pl.LightningModule):
 
         # networks
         data_shape = (channels, width, height)
-        self.generator = Generator(
-            latent_dim=self.hparams.latent_dim, num_features=self.hparams.num_features, img_shape=data_shape)
-        self.discriminator = Discriminator(
-            num_features=self.hparams.num_features, img_shape=data_shape)
+        self.generator = self._get_generator(
+            data_shape, GeneratorClass=Generator)
+        self.discriminator = self._get_discriminator(
+            data_shape, DiscriminatorClass=Discriminator)
 
         self.validation_z = torch.randn(8, self.hparams.latent_dim, 1, 1)
 
@@ -40,72 +45,100 @@ class conditionalGAN(pl.LightningModule):
         self.example_feature_array = torch.zeros(
             8, self.hparams.num_features)
 
+    def _get_generator(self, data_shape, GeneratorClass) -> nn.Module:
+        generator = GeneratorClass(latent_dim=self.hparams.latent_dim,
+                                   num_features=self.hparams.num_features, img_shape=data_shape)
+        generator.apply(self._weights_init)
+        return generator
+
+    def _get_discriminator(self, data_shape, DiscriminatorClass) -> nn.Module:
+        discriminator = DiscriminatorClass(
+            num_features=self.hparams.num_features, img_shape=data_shape)
+        discriminator.apply(self._weights_init)
+        return discriminator
+
+    @staticmethod
+    def _weights_init(m):
+        classname = m.__class__.__name__
+        if classname == 'Conv2d' or classname == 'ConvTranspose2d':
+            torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        elif classname.find("BatchNorm") != -1:
+            torch.nn.init.normal_(m.weight, 1.0, 0.02)
+            torch.nn.init.zeros_(m.bias)
+
     def forward(self, z, features=None):
         if features is None:
             features = self.example_feature_array.type_as(z)
-        return self.discriminator(self.generator(z, features), features)
+        generated_images = self.generator(z, features)
+        dicriminator_decision = self.discriminator(generated_images, features)
+        return generated_images
 
     def adversarial_loss(self, y_hat, y):
         return F.binary_cross_entropy(y_hat, y)
 
-    def training_step(self, batch, batch_idx, optimizer_idx):
-        imgs, features = batch
-        # sample noise
+    def _generator_step(self, real_imgs, features):
         z = torch.randn(
-            imgs.shape[0], self.hparams.latent_dim, 1, 1).type_as(imgs)
+            real_imgs.shape[0], self.hparams.latent_dim, 1, 1).type_as(real_imgs)
+
+        # ground truth result (ie: all fake)
+        # put on GPU because we created this tensor inside training_loop
+        valid_ground_truth = torch.ones(
+            real_imgs.size(0), 1).type_as(real_imgs)
+        valid_ground_truth = valid_ground_truth
+
+        # adversarial loss is binary cross-entropy
+        g_loss = self.adversarial_loss(
+            self.discriminator(self.generator(z, features), features), valid_ground_truth)
+        self.log('train/g_loss', g_loss, on_epoch=True,
+                 on_step=True, logger=True, prog_bar=True)
+        print(z.requires_grad, real_imgs.requires_grad, )
+        return g_loss
+
+    def _discriminator_step(self, real_imgs, features):
+        '''Measure discriminator's ability to classify real from generated samples'''
+        z = torch.randn(
+            real_imgs.shape[0], self.hparams.latent_dim, 1, 1).type_as(real_imgs)
+
+        # ground truth result (ie: all fake)
+        # how well can it label as real?
+        valid_ground_truth = randomly_flip_labels(
+            torch.ones(real_imgs.size(0), 1), p=self.hparams.label_flipping_p).type_as(real_imgs)
+        fake_ground_truth = randomly_flip_labels(
+            torch.zeros(real_imgs.size(0), 1), p=self.hparams.label_flipping_p).type_as(real_imgs)
+
+        real_loss = self.adversarial_loss(
+            self.discriminator(real_imgs, features), valid_ground_truth)
+
+        fake_loss = self.adversarial_loss(
+            self.discriminator(self.generator(z, features).detach(), features), fake_ground_truth)
+
+        # discriminator loss is the average of these
+        d_loss = (real_loss + fake_loss) / 2
+        self.log('train/d_loss', d_loss, on_epoch=True,
+                 on_step=True, prog_bar=True)
+        return d_loss
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        real_imgs, features = batch
+        # train discriminator
+        if optimizer_idx == 0:
+            return self._discriminator_step(real_imgs, features)
 
         # train generator
-        if optimizer_idx == 0:
-            # ground truth result (ie: all fake)
-            # put on GPU because we created this tensor inside training_loop
-            valid = torch.ones(imgs.size(0), 1)
-            valid = valid.type_as(imgs)
-
-            # adversarial loss is binary cross-entropy
-            g_loss = self.adversarial_loss(
-                self.discriminator(self.generator(z, features), features), valid)
-
-            self.log('train/g_loss', g_loss, on_epoch=True,
-                     on_step=True, logger=True, prog_bar=True)
-            return g_loss
-
-        # train discriminator
         if optimizer_idx == 1:
-            # Measure discriminator's ability to classify real from generated samples
-
-            # how well can it label as real?
-            valid = torch.ones(imgs.size(0), 1)
-            valid = randomly_flip_labels(valid, p=0.2)
-            valid = valid.type_as(imgs)
-
-            real_loss = self.adversarial_loss(
-                self.discriminator(imgs, features), valid)
-
-            # how well can it label as fake?
-            fake = torch.zeros(imgs.size(0), 1)
-            fake = randomly_flip_labels(fake, p=0.2)
-            fake = fake.type_as(imgs)
-
-            fake_loss = self.adversarial_loss(
-                self.discriminator(self.generator(z, features).detach(), features), fake)
-
-            # discriminator loss is the average of these
-            d_loss = (real_loss + fake_loss) / 2
-            self.log('train/d_loss', d_loss, on_epoch=True,
-                     on_step=True, prog_bar=True)
-            return d_loss
+            return self._generator_step(real_imgs, features)
 
     def configure_optimizers(self):
         lr = self.hparams.lr
-        b1 = 0.5  # self.hparams.b1
-        b2 = 0.99  # self.hparams.b2
+        b1 = self.hparams.b1
+        b2 = self.hparams.b2
 
         # This is supposed to be better for GANs than Adam
         opt_g = torch.optim.Adam(
             self.generator.parameters(), lr=lr, betas=(b1, b2))
         opt_d = torch.optim.Adam(
             self.discriminator.parameters(), lr=lr, betas=(b1, b2))
-        return [opt_g, opt_d], []
+        return [opt_d, opt_g], []
 
     def on_epoch_end(self):
         z = self.validation_z.to(self.device)
--- src/train.py
+++ src/train.py
@@ -15,28 +15,33 @@ import wandb
 
 parser = argparse.ArgumentParser("Parsing training arguments")
 
-parser.add_argument('--lr', type=float, default=0.00001)
-parser.add_argument('--epochs', '-e', type=int, default=20)
-parser.add_argument('--workers', '-w', type=int, default=4)
-parser.add_argument('--batch-size', '-b', type=int, default=4)
+
 parser.add_argument('--data-dir', '-d', type=str,
                     default="./data/wikiart-emotions")
+parser.add_argument('--gpus', type=int, nargs='+', default=-1,
+                    help="specify gpus to use. default is to use all available")
+parser.add_argument('--cpu', action='store_true',
+                    help="use cpu instead of gpu")
+parser.add_argument('--workers', '-w', type=int, default=4)
+##################### ------------------ #####################
+parser.add_argument('--image-resizing', '-i', type=int, default=256)
+parser.add_argument('--lr', type=float, default=0.00001)
+parser.add_argument('--epochs', '-e', type=int, default=20)
+parser.add_argument('--batch-size', '-b', type=int, default=4)
 parser.add_argument('--num-features', '-f', type=int, default=60)
 parser.add_argument('--latent-dim', '-l', type=int, default=200)
+parser.add_argument('--label-flipping-p', '--lfp', type=float, default=0,
+                    help="Probability for label flipping. Set to zero to disable.")
+###################### ------------------ #####################
 parser.add_argument('--results-dir', '-c', type=str,
                     default=f"./results/")
 parser.add_argument('--training-name', '-n', type=str,
                     help='Name you want to use for this training run, will be used in '
                          'log and model saving.', default=datetime.now().strftime('%d-%m-%Y_%H_%M_%S'))
-parser.add_argument('--image-resizing', '-i', type=int, default=256)
 parser.add_argument('--no-wandb', action="store_true",
                     help="Disable logging to wandb")
 parser.add_argument('--tags', type=str, nargs='+',
                     default=[], help="Tag this run in wandb.")
-parser.add_argument('--gpus', type=int, nargs='+', default=-1,
-                    help="specify gpus to use. default is to use all available")
-parser.add_argument('--cpu', action='store_true',
-                    help="use cpu instead of gpu")
 parser.add_argument('--fast-debug', action="store_true",
                     help="do a fast run through the code to check for errors")
 
@@ -64,7 +69,8 @@ if __name__ == '__main__':
     dm = WikiArtEmotionsDataModule(
         config.data_dir, config.batch_size, config.workers, config.image_resizing, fast_debug=config.fast_debug)
     model = conditionalGAN(*dm.size(), lr=config.lr,
-                           batch_size=config.batch_size, latent_dim=config.latent_dim, num_features=config.num_features)
+                           batch_size=config.batch_size, latent_dim=config.latent_dim,
+                           num_features=config.num_features, label_flipping_p=config.label_flipping_p)
 
     if config.no_wandb:
         os.environ["WANDB_MODE"] = "dryrun"
