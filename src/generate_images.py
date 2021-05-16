439179ee4a96009e97a37c29ca440134a5e9ffc7
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-05-16 13:11:40 +0200

    improve conditional generation script for celeba

--- src/generate_images.py
+++ src/generate_images.py
@@ -10,6 +10,7 @@ import torchvision
 import wandb
 from emotion_classifier.emotion_classifier import EmotionClassifier
 from src.gan.outer_gan import GAN, WGAN_GP
+from src.common.condition_helpers import emotions, faces
 
 class_names = {
     "gan": GAN,
@@ -34,8 +35,8 @@ parser.add_argument('--num-images', '-n', type=int,
                     default=64, help="Number of images to generate.")
 parser.add_argument('--output-image', '-o', type=str,
                     default="output/image.png", help="Location of output image.")
-parser.add_argument('--condition-file', type=str,
-                    help="parse conditions from file")
+parser.add_argument('--condition-template', type=str, default="emotions",
+                    help="parse conditions from file OR use to specify conditions type: emotions or faces")
 parser.add_argument('--conditions', type=str, nargs="+", default=[],
                     help="specify conditions by cmd arg")
 
@@ -48,27 +49,6 @@ def loadAttributes(attributesPath):
     attributes[attributes == 0] = -1
     return attributes
 
-
-emotions = OrderedDict([("agreeableness", -1),
-                        ("anger", -1),
-                        ("anticipation", -1),
-                        ("arrogance", -1),
-                        ("disagreeableness", -1),
-                        ("disgust", -1),
-                        ("fear", -1),
-                        ("gratitude", -1),
-                        ("happiness", -1),
-                        ("humility", -1),
-                        ("love", -1),
-                        ("optimism", -1),
-                        ("pessimism", -1),
-                        ("regret", -1),
-                        ("sadness", -1),
-                        ("shame", -1),
-                        ("shyness", -1),
-                        ("surprise", -1),
-                        ("trust", -1),
-                        ("neutral", -1)])
 
 if __name__ == "__main__":
     # TODO: seed
@@ -95,7 +75,16 @@ if __name__ == "__main__":
     latent_dim = model.hparams.latent_dim
 
     z = torch.randn(config.num_images, latent_dim, 1, 1)
-    if config.condition_file:
+    if config.conditions:
+        condition_template = emotions if config.condition_template == "emotions" else faces
+        for condi in config.conditions:
+            condition_template[condi] = 1
+        labels = torch.cat(config.num_images *
+                           [torch.FloatTensor(list(condition_template.values()))])
+        if config.output_image == "output/image.png":
+            config.output_image = "output/" + \
+                "_".join(config.conditions) + ".png"
+    elif config.condition_file:
         labels = torch.cat(config.num_images *
                            [loadAttributes(config.condition_file)])
     elif config.conditions:
