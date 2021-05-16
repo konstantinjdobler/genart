d1f77972bef8523d42a83834ef0fd024636ebfdc
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-05-16 13:05:51 +0200

    conditional image generation

--- src/generate_images.py
+++ src/generate_images.py
@@ -1,6 +1,7 @@
 # Fix imports and prevent formatting
 import sys  # nopep8
-from os.path import dirname, join, abspath  # nopep8
+from os.path import dirname, join, abspath
+from typing import OrderedDict  # nopep8
 sys.path.insert(0, abspath(join(dirname(__file__), '..')))  # nopep8
 
 import argparse
@@ -33,12 +34,54 @@ parser.add_argument('--num-images', '-n', type=int,
                     default=64, help="Number of images to generate.")
 parser.add_argument('--output-image', '-o', type=str,
                     default="output/image.png", help="Location of output image.")
+parser.add_argument('--condition-file', type=str,
+                    help="parse conditions from file")
+parser.add_argument('--conditions', type=str, nargs="+", default=[],
+                    help="specify conditions by cmd arg")
 
+
+def loadAttributes(attributesPath):
+    with open(attributesPath) as file:
+        lines = [line.rstrip() for line in file]
+    attributes = torch.FloatTensor(
+        [float(line.split(': ')[-1]) for line in lines])
+    attributes[attributes == 0] = -1
+    return attributes
+
+
+emotions = OrderedDict([("agreeableness", -1),
+                        ("anger", -1),
+                        ("anticipation", -1),
+                        ("arrogance", -1),
+                        ("disagreeableness", -1),
+                        ("disgust", -1),
+                        ("fear", -1),
+                        ("gratitude", -1),
+                        ("happiness", -1),
+                        ("humility", -1),
+                        ("love", -1),
+                        ("optimism", -1),
+                        ("pessimism", -1),
+                        ("regret", -1),
+                        ("sadness", -1),
+                        ("shame", -1),
+                        ("shyness", -1),
+                        ("surprise", -1),
+                        ("trust", -1),
+                        ("neutral", -1)])
 
 if __name__ == "__main__":
     # TODO: seed
     config = parser.parse_args()
     print(config)
+
+    def loadAttributes(attributesPath):
+        with open(attributesPath) as file:
+            lines = [line.rstrip() for line in file]
+        attributes = torch.FloatTensor(
+            [float(line.split(': ')[-1]) for line in lines])
+        attributes[attributes == 0] = -1
+        return attributes
 
     checkpoint_file = config.checkpoint
     if config.wandb:
@@ -52,7 +95,19 @@ if __name__ == "__main__":
     latent_dim = model.hparams.latent_dim
 
     z = torch.randn(config.num_images, latent_dim, 1, 1)
-    labels = model.example_label_array
+    if config.condition_file:
+        labels = torch.cat(config.num_images *
+                           [loadAttributes(config.condition_file)])
+    elif config.conditions:
+        for condi in config.conditions:
+            emotions[condi] = 1
+        labels = torch.cat(config.num_images *
+                           [torch.FloatTensor(list(emotions.values()))])
+        if config.output_image == "output/image.png":
+            config.output_image = "output/" + \
+                "_".join(config.conditions) + ".png"
+    else:
+        labels = model.example_label_array
     output = model.generator(z, labels)
     grid = torchvision.utils.make_grid(output)
     img = wandb.Image(grid)
