6ce82286d909ecfb5c4905d372da0f4ef09d91fc
Konstantin Dobler <konstantin.j.dobler@gmail.com>
2021-05-16 13:44:26 +0200

    test4

--- src/gan/outer_gan.py
+++ src/gan/outer_gan.py
@@ -217,8 +217,8 @@ class GAN(pl.LightningModule):
         return [opt_d, opt_g], []
 
     def on_train_epoch_end(self, outputs):
-        epoch_length = len(outputs)
-        print(epoch_length, outputs)
+        epoch_length = len(outputs[0])
+        print(epoch_length)
         # Don't log every epoch, it's too much... maybe a cmd arg later on
         # bigger image size => longer epochs => we can log more often without rate limits
         log_interval = int((64 * 30) / self.hparams.width)
