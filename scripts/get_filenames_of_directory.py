ce6f587f68d759faa4692257e5c629f65a16aba7
smilef4ce <christian.huebschi@gmail.com>
2021-05-31 23:30:38 +0200

    added constraints functionality

--- scripts/get_filenames_of_directory.py
+++ scripts/get_filenames_of_directory.py
@@ -7,13 +7,17 @@ parser.add_argument('--data_dir', '-d', type=str, default="./",
                     help="Specifiy the directory name with files")
 parser.add_argument('--file', '-f', type=str, default="downloaded_files.txt",
                     help="Specifiy the file name for output file")
-parser.add_argument('--constraint', '-c', type=str, default=".jpg",
-                    help="Specifiy post fix for file names such as png/jpg")
+parser.add_argument('--prefix-constraint', '-pre', type=str, default=".",
+                    help="Specifiy a prefix to skip file names starting with certain symbals such as . for hidden files")
+parser.add_argument('--postfix-constraint', '-post', type=str, default=".jpg",
+                    help="Specifiy a postfix to only match file names with a certain file extension such as .jpg")
 
 config = parser.parse_args()
 print(config)
 
 write_file = config.file
+prefix = config.prefix_constraint
+postfix = config.post_constraint
 
 f = open(write_file, "w")
 f.close()
@@ -21,5 +25,5 @@ f.close()
 files = os.listdir(config.data_dir)
 with open(write_file, "a") as output_file:
     for file in files:
-        if file[-len(config.constraint):] == ".png" and file[0] != ".":
+        if file[-len(postfix):] == postfix and file[len(prefix)] != prefix:
             output_file.write(f'{file}\n')
